51a6bea721e41bddd890677cdb9d5cd63cd6b8e9
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-09-18 16:23:25 -0300

    Generate contract service by feathers cli

---
+++ api/src/models/contract.model.js
@@ -0,0 +1,28 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const contract = sequelizeClient.define('contract', {
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  contract.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return contract;
+};
