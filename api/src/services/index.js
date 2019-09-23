d10200752abdd6239e08a46b797031d4836b0947
André Giampaolo <andregiampaolo@gmail.com>
2019-09-23 13:31:14 -0300

    Generate role service with feathers cli

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -1,9 +1,11 @@
 const users = require('./users/users.service.js');
 const contract = require('./contract/contract.service.js');
 const subject = require('./subject/subject.service.js');
+const role = require('./role/role.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(contract);
   app.configure(subject);
+  app.configure(role);
 };
