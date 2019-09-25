5050c81a2e0558877ac7ab89d78377bc5d6277f8
André Giampaolo <andregiampaolo@gmail.com>
2019-09-25 14:15:21 -0300

    WIP: verify user has permission to access this function

--- api/src/services/subject/subject.hooks.js
+++ api/src/services/subject/subject.hooks.js
@@ -4,7 +4,33 @@ const checkPermissions = require('feathers-permissions');
 
 module.exports = {
   before: {
-    all: [authenticate('jwt'), checkPermissions({ roles: ['admin'] })],
+    all: [authenticate('jwt'), checkPermissions({
+      async roles(context) {
+
+        const { user } = context.params;
+        const roles = await context.app.service('role').find({
+          query: {
+            id: user.roleId
+          }
+        });
+
+        context.params.user['permissions'] = 'subject:find,subject:create';//roles.data.map(role => role.slug);
+
+        console.log('Caminho: ', context.params.user['permissions']);
+        console.log('Path: ', context.path + ':' + context.method);
+
+        return [context.path + ':' + context.method];
+
+
+        // const role = await context.app.service('role').get(context.params.user.roleId);
+        // console.log(role);
+        // // async (context) => {
+
+        // //   return checkPermissions({ roles: [role.slug] });
+        // // }
+        // return ['admin'];
+      }
+    })],
     find: [],
     get: [],
     create: [],
