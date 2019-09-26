c721d97654c411869120658128be3aeedb67ca50
André Giampaolo <andregiampaolo@gmail.com>
2019-09-26 13:41:59 -0300

    Create a files to seed in database

---
+++ api/seed/permissions.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    'path': 'subject',
+    'method': 'find'
+  },
+  {
+    'path': 'subject',
+    'method': 'get'
+  },
+  {
+    'path': 'subject',
+    'method': 'create'
+  },
+  {
+    'path': 'subject',
+    'method': 'update'
+  },
+  {
+    'path': 'subject',
+    'method': 'patch'
+  },
+  {
+    'path': 'subject',
+    'method': 'remove'
+  },
+  {
+    'path': 'contract',
+    'method': 'find'
+  },
+  {
+    'path': 'contract',
+    'method': 'get'
+  },
+  {
+    'path': 'contract',
+    'method': 'create'
+  },
+  {
+    'path': 'contract',
+    'method': 'update'
+  },
+  {
+    'path': 'contract',
+    'method': 'patch'
+  },
+  {
+    'path': 'contract',
+    'method': 'remove'
+  }
+];
---
+++ api/seed/roles.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    'name': 'Administrador',
+    'slug': 'admin'
+  },
+  {
+    'name': 'Sponsor',
+    'slug': 'sponsor'
+  },
+  {
+    'name': 'Teacher',
+    'slug': 'teacher'
+  }
+];
