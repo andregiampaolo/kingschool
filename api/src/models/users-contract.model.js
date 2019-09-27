8eaafbb9ab1846467b569c7079403bf1dc4d83b7
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-09-27 18:15:15 -0300

    Create a model work with association users and contract

---
+++ api/src/models/users-contract.model.js
@@ -0,0 +1,23 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+const sequelizeClient = app.get('sequelizeClient');
+  const usersContract = sequelizeClient.define('usersContract', {
+    accept: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  return usersContract;
+}
